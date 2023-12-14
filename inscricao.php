cbf3a3f8a471f42b1047863dbcf4ab300ab2ed31
Gustavo Viana Calderaro Nogueira <gnogueira@seplan.intranet>
2023-12-14 10:16:10 -0300

    update

--- inscricao.php
+++ inscricao.php
@@ -7,9 +7,10 @@ include('pdo.php');
   <meta charset="utf-8" name="viewport">   
   <title>Inscrição</title>  
   <link rel="stylesheet" href="css/nicepage.css" media="screen"> 
-  <link rel="stylesheet" href="css/responsivo.css"> 
+  <link rel="stylesheet" href="css/responsivo.css?<?php echo rand(1, 1000);?>"> 
   <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
   <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">     
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">  
 </head>
 <body>  
 <div class="u-clearfix u-sheet u-sheet-1">
@@ -39,17 +40,19 @@ include('pdo.php');
                     $fim = $dados['fim'];                               
                     }
                   }
-                }  
-                $date_now = date("Y-m-d");
-                date_default_timezone_set("America/Bahia");
-                $hora = date("H:i:s");  
-                if($date_now == $data and $hora > $fim)
-                {
-                 header("Location: mensagem.php");   
-                } else if($date_now > $data)
-                {
-                 header("Location: mensagem.php");   
-                }
+                  $date_now = date("Y-m-d");
+                  date_default_timezone_set("America/Bahia");
+                  $hora = date("H:i:s");  
+                  if($date_now == $data and $hora > $fim)
+                  {
+                   header("Location: mensagem.php");   
+                  } else if($date_now > $data)
+                  {
+                   header("Location: mensagem.php");   
+                  }
+                } else {
+                  header("Location: mensagem2.php");  
+                }             
               ?>  
            <br><br>           
            <?php
@@ -68,10 +71,12 @@ include('pdo.php');
                       $fim = $dados['fim']; 
                     ?>
                     <div class="div-texto">  
-                      <h3 class="u-text u-text-1">Página de Inscrição</h3>
-                      <h3 class="u-text u-text-1"><?php echo $nome;?></h3>              
-                      <h4 class="u-text u-text-1">Data: <?php echo $data;?></h4>
-                      <h4 class="u-text u-text-1">Horário: <?php echo $inicio;?> às <?php echo $fim; ?></h4>
+                      <h4 class="u-text u-text-1 texto-1">CHECKING SEPLAN</h4>
+                      <h4 class="u-text u-text-1 texto-2">CONFIRME AQUI A SUA PARTICIPAÇÃO</h4>
+                      <h3 class="u-text u-text-1 u-text-palette-2-base texto-3"><?php echo $nome;?></h3>              
+                      <h4 class="u-text u-text-1 texto-4">Data: <?php echo $data;?></h4>
+                      <h4 class="u-text u-text-1 texto-5">Horário: <?php echo date('h:i', strtotime($inicio)); ?>
+                      às <?php echo date('h:i', strtotime($fim)); ?></h4>
                       <br><br>
                     </div>  
                    <?php   
@@ -105,29 +110,28 @@ include('pdo.php');
                <label for="name-6715" class="u-label">Nome:</label>
                <input type="text" placeholder="Digite seu nome" id="nome" name="nome" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required>
               </div>
-              <br><br>  
+              <br>
               <div class="u-form-group u-form-name u-label-top">
                <label for="name-6715" class="u-label">RG:</label>
                <input type="text" placeholder="Digite seu rg" id="rg" name="rg" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required>
               </div>
-              <br><br> 
+              <br>
               <div class="u-form-group u-form-name u-label-top">
                <label for="name-6715" class="u-label">CPF:</label>
                <input type="text" placeholder="Digite seu cpf" id="cpf" name="cpf" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required>
               </div>
-              <br><br>             
+              <br>       
               <div class="u-form-group u-form-name u-label-top">
                 <label for="name-6715" class="u-label">Email:</label>
                 <input type="text" placeholder="Digite seu email" id="email" name="email" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required>
               </div>
-              <br><br>
+              <br>
               <div class="u-form-group u-form-name u-label-top">
                <label for="name-6715" class="u-label">Telefone:</label>
                <input type="text" placeholder="Digite seu telefone" id="telefone" name="telefone" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required>
                <br>              
                <label for="name-6715" class="u-label">Selecione seu Órgão:</label>
-              </div>
-              <br>                
+              </div>                           
               <div class="u-form-group u-form-name u-label-top">              
                     <select name="orgao" class="menu">                    
                     <?php
@@ -144,12 +148,12 @@ include('pdo.php');
                     ?>
                     </select>                    
              </div> 
-             <br><br> 
+             <br> 
              <div class="u-form-group u-form-name u-label-top">
                <label for="name-6715" class="u-label">Cargo:</label>
                <input type="text" placeholder="Digite seu cargo" id="cargo" name="cargo" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required>
               </div>
-              <br><br>                              
+              <br>                              
              <div class="u-align-left u-form-group u-form-submit u-label-top">                          
                <input type="submit" value="Inscrever-se" name="inscrever" id="inscrever" class="u-btn u-btn-submit u-button-style">                  				     
              </div>
@@ -165,19 +169,36 @@ include('pdo.php');
     margin-left: 29px;
     margin-top: 20px;
     }
-    .u-text-1
+    .texto-1
     {
-      font-size: 30px;   
+      font-size: 35px;   
+    }
+    .texto-2
+    {
+      font-size: 35px;
+    }
+    .texto-3
+    {
+      font-size: 32px;
+    }
+    .texto-4
+    {
+      font-size: 29px;
+    }
+    .texto-5
+    {
+      font-size: 29px;
     }
     .fonte
     {
-    font-size: 35px;  
+    font-size: 38px;  
     }
     .menu
     {
     width:200px;
     height:38px;  
     margin-left: 30px;
+    margin-top: 9px;
     }  
     .u-border-1
     {     
