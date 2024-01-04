f1dd6b5cbfb6404f32dbb795e3a429e25483d96b
Gustavo Viana Calderaro Nogueira <gnogueira@seplan.intranet>
2024-01-04 11:13:13 -0300

    update

--- js/validacao.js
+++ js/validacao.js
@@ -1,6 +1,15 @@
 function isValidCPF(cpf) {
   cpf = cpf.replace(/[^\d]+/g, '');
   if (cpf === '00000000000') return false; 
+  if (cpf === '11111111111') return false; 
+  if (cpf === '22222222222') return false; 
+  if (cpf === '33333333333') return false; 
+  if (cpf === '44444444444') return false; 
+  if (cpf === '55555555555') return false; 
+  if (cpf === '66666666666') return false;
+  if (cpf === '77777777777') return false;
+  if (cpf === '88888888888') return false;
+  if (cpf === '99999999999') return false;    
   if (cpf.length !== 11) return false;
 
   let sum = 0;
@@ -35,9 +44,15 @@ function validarCPFInput() {
   const cpfInput = $('#cpf').val();
   const isValid = isValidCPF(cpfInput);
 
-  if (isValid  || cpfInput === '000.000.000-00'){
+  if (isValid  || cpfInput === '000.000.000-00' || cpfInput === '111.111.111-11' || cpfInput === '222.222.222-22'
+  || cpfInput === '333.333.333-33' || cpfInput === '444.444.444-44' || cpfInput === '555.555.555-55'
+  || cpfInput === '666.666.666-66' || cpfInput === '777.777.777-77' || cpfInput === '888.888.888-88'
+  || cpfInput === '999.999.999-99'){
     $('#cpf').get(0).setCustomValidity('');
-    if (cpfInput === '000.000.000-00'){      
+    if (cpfInput === '000.000.000-00' || cpfInput === '000.000.000-00' || cpfInput === '111.111.111-11' || cpfInput === '222.222.222-22'
+    || cpfInput === '333.333.333-33' || cpfInput === '444.444.444-44' || cpfInput === '555.555.555-55'
+    || cpfInput === '666.666.666-66' || cpfInput === '777.777.777-77' || cpfInput === '888.888.888-88'
+    || cpfInput === '999.999.999-99'){      
       $('#cpf').prop('required', true);
     } else {
       $('#cpf').prop('required', false);
